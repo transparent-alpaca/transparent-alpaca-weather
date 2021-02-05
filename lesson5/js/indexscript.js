e1373e96e81811e340cac7dbecbaeab615e45b4d
transparent-alpaca <rio19001@byui.edu>
2021-02-04 23:53:17 -0700

    preston 5

---
+++ lesson5/js/indexscript.js
@@ -0,0 +1,48 @@
+function getYear() {
+  var d = new Date();
+  var n = d.getFullYear();
+  document.getElementById("year").innerHTML = n;
+}
+
+function getLastModified() {
+    var lm = document.lastModified
+    document.getElementById("lastupdated").innerHTML = lm;
+}
+
+function getCurrentDay() {
+  let dayname = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+];
+
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "Nomberber",
+    "December",
+];
+let d = new Date();
+let dName = dayname[d.getDay()];
+let dMonth = months[d.getMonth()];
+let fulldate = dName + ", " + d.getDate() + " " + dMonth + " " + d.getFullYear();
+document.getElementById("currentdate").innerHTML = fulldate;
+}
+
+function start() {
+    getYear();
+    /*getLastModified();*/
+    getCurrentDay();
+}

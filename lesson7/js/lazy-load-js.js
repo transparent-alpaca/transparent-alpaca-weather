f54742f59ecc962a6b8f75f8ef4f19f8d44be763
transparent-alpaca <rio19001@byui.edu>
2021-02-18 23:29:37 -0700

    lesson7 images

--- lesson7/js/lazy-load-js.js
+++ lesson7/js/lazy-load-js.js
@@ -11,7 +11,7 @@ window.addEventListener('load',(event)=>{
     }
     const imgOptions = {
         threshold: 0,
-        rootMargin: "0px 0px -300px 0px"
+        rootMargin: "0px 0px -200px 0px"
     }
     const imgObserver = new IntersectionObserver((entries,imgObserver)=> {
         console.log(entries);

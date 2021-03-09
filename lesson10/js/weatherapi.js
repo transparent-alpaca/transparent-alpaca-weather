a30fb577395c981d313830f0c9d7d0f17bb8b1cf
transparent-alpaca <rio19001@byui.edu>
2021-03-09 09:25:07 -0700

    api

--- lesson10/js/weatherapi.js
+++ lesson10/js/weatherapi.js
@@ -1,11 +1,17 @@
-const apiURL = 'http://api.openweathermap.org/data/2.5/forecast?id=5604473&appid=e77db52f2da17d60e264bb2bf0f2fe37&units=imperial';
+const apiURL = 'http://api.openweathermap.org/data/2.5/weather?id=5604473&appid=e77db52f2da17d60e264bb2bf0f2fe37&units=imperial';
 
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
 
+        const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.weather[0].icon + '.png';
+        const desc = jsObject.weather[0].description;
+        document.getElementById('imagesrc').textContent = imagesrc;
+        document.getElementById('icon').setAttribute('src', imagesrc);
+        document.getElementById('icon').setAttribute('alt', desc);
 
-        document.getElementById('current-temp').textContent=jsObject.list[0].main.temp;
+
+        document.getElementById('current-temp').textContent=jsObject.main.temp;
 
     console.log(jsObject);
 
